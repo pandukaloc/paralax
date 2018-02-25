0af7056549896ac2e2c3a47877d7fae96e7cff26
pandukaloc <panduka29@gmail.com>
2018-02-25 23:33:26 +0530

    New changelist

--- modules/mod_tictok_home_features/helper.php
+++ modules/mod_tictok_home_features/helper.php
@@ -6,24 +6,63 @@ class modfeatures
 
     public function getHtml()
     {
-       $query = $db->getQuery(true)
-            ->select($db->quoteName(array('c.id', 'c.title', 'c.introtext', 'c.images','ca.alias' ), array('id', 'title', 'introtext', 'images', 'caalias')))
+        $db = JFactory::getDbo();
 
-            ->from($db->quoteName('#__content', 'c', '#__categories','ca'))  
-               ->join('INNER', $db->quoteName('#__categories', 'ca') . ' ON (' . $db->quoteName('c.catid') . ' = ' . $db->quoteName('ca.id') . ')')
-                           
-            ->where($db->quoteName('c.catid') . '='.$db->Quote($categoryid) . ' AND ' . $db->quoteName('c.state') . '=1' )
-            ->order($db->quoteName('c.ordering') . ' ASC');
+        $query = $db->getQuery(true)
+            ->select($db->quoteName(array('m.id', 'm.title', 'm.alias', 'm.params', 'm.type', 'm.link', 'm.path'), array('id', 'title', 'alias', 'params', 'type', 'link', 'path')))
+            ->from($db->quoteName('#__menu', 'm'))
+            ->where($db->quoteName('m.menutype') . '="mainmenu"' . ' AND ' . $db->quoteName('m.published') . '=1' . ' AND ' . $db->quoteName('m.level') . '=1' )
+            ->order($db->quoteName('m.lft') . ' ASC');
 
 
         $db->setQuery($query);
-        $items = ($items = $db->loadObjectList())?$items:array();
-        return $items;
- 
+        $items = $db->loadObjectList();
+
+        $menuItem = "<ul class=\"nav navbar-nav\" id=\"mainNav\">";
+
+        foreach ($items as $item) {
+            if ($db->loadResult()) {
+                $menuItem .= " <li class=\"active\" id=\"firstLink\">";
+                $menuItem .= "<a href=\"#\" class=\"scroll-link\">$item->title</a>";
+                $menuItem .= "</li>";
 
 
+            }
 
-}
+        }
+
+        $menuItem.="</ul>";
+        return $menuItem;
+
+    }
+
+    public function getHtmlmob()
+    {
+        $db = JFactory::getDbo();
+
+        $query = $db->getQuery(true)
+            ->select($db->quoteName(array('m.id', 'm.title', 'm.alias', 'm.params', 'm.type', 'm.link', 'm.path'), array('id', 'title', 'alias', 'params', 'type', 'link', 'path')))
+            ->from($db->quoteName('#__menu', 'm'))
+            ->where($db->quoteName('m.menutype') . '="mainmenu"' . ' AND ' . $db->quoteName('m.published') . '=1' . ' AND ' . $db->quoteName('m.level') . '=2' . ' AND ' . $db->quoteName('m.id') . '!=101')
+            ->order($db->quoteName('m.lft') . ' ASC');
+
+
+        $db->setQuery($query);
+        $items = $db->loadObjectList();
+        $menuItem = "<ul class=\"nav navbar-nav\" id=\"mainNav\">";
+        foreach ($items as $item) {
+            if ($db->loadResult()) {
+                $menuItem .= " <li class=\"active\" id=\"firstLink\">";
+                $menuItem .= "<a href=\"#\" class=\"scroll-link\">$item->title</a>";
+                $menuItem .= "</li>";
+                $menuItem .= "</a>";
+
+
+            }
+        }
+        return $menuItem;
+    }
+
 }
 
 ?>
